9ce4d6fccc02e1192e36e484c5fd6ceb48dd45e4
Shumansky Alexey <alexey.shumansky@gmail.com>
2019-01-28 22:13:45 +0300

    fead: add filter to Photograpers

--- src/components/List.js
+++ src/components/List.js
@@ -1,18 +1,42 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import PhotographerCard from "./PhotograperCard";
-import { Row, Col } from "reactstrap";
+import { Row, Col, Input } from "reactstrap";
 
-export default class List extends Component {
+class List extends Component {
+  state = { value: "" };
+
+  change = e => {
+    this.setState({ value: e.target.value });
+  };
+
   render = () => {
     const { data, match } = this.props;
+    const inputValue = this.state.value.toLocaleLowerCase();
+    const photograper = data
+      .filter(
+        x =>
+          x.person.toLocaleLowerCase().includes(inputValue) ||
+          x.motherland.toLocaleLowerCase().includes(inputValue)
+      )
+      .map(photographer => (
+        <Col md="3" key={photographer.id}>
+          <PhotographerCard route={match.url} data={photographer} />
+        </Col>
+      ));
     return (
-      <Row>
-        {data.map(photographer => (
-          <Col md="3" key={photographer.id}>
-            <PhotographerCard route={match.url} data={photographer} />
-          </Col>
-        ))}
-      </Row>
+      <Fragment>
+        <Input
+          type="search"
+          name="search"
+          placeholder="Photograper name or place"
+          style={{ marginBottom: "30px", width: "300px" }}
+          onChange={this.change}
+          value={this.state.value}
+        />
+        <Row>{photograper}</Row>
+      </Fragment>
     );
   };
 }
+
+export default List;
--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
+import no_photo from "./images/no_photo.jpg";
 import img0 from "./images/0.jpg";
 import img1 from "./images/1.jpg";
-import no_photo from "./images/no_photo.jpg";
 import img3 from "./images/3.jpg";
 import img4 from "./images/4.jpg";
 import img5 from "./images/5.jpg";
